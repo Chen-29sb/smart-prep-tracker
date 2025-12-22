cac1878966e9c1e43c060e650fc73788dd6c46a9
Chen-29sb <shristibhomrajka.29@gmail.com>
2025-12-22 20:51:48 +0530

    feat: setup express server with health check route

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is running fine 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
