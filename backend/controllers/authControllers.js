f639d3d2648fd1e7ceb748939fade14c3b8f11a4
Chen-29sb <shristibhomrajka.29@gmail.com>
2025-12-24 19:19:16 +0530

    feat: implement login API with JWT authentication

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+//Register
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -37,9 +39,54 @@ const registerUser = async (req, res) => {
       },
     });
   } catch (error) {
-    console.error("Register error:", error.message);
     res.status(500).json({ message: "Server error" });
   }
 };
 
-module.exports = { registerUser };
+//Login
+
+const loginUser = async(req, res) => {
+    try{
+        const{email, password} = req.body;
+
+        // 1. Validate
+        if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+        }
+
+        // 2. Check User
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // 3. Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        // 4. Generate token
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        // 5. Response
+        res.status(200).json({
+        message: "Login successful",
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        },
+        });
+    } catch (error) {
+    res.status(500).json({ message: "Server error" });
+    }
+};
+
+
+module.exports = { registerUser, loginUser };
